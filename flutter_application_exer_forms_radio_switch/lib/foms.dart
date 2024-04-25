61e819d600b10af743b0466fa52c469221e00fbd
Jerso06 <jedsonhenrique2006@gmail.com>
2024-04-25 18:54:45 -0300

    Finalizando o exercício Class/Radio/Switch

--- flutter_application_exer_forms_radio_switch/lib/foms.dart
+++ flutter_application_exer_forms_radio_switch/lib/foms.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
-
 import 'package:flutter/material.dart';
+import 'package:flutter_application_exer_forms_radio_switch/interessado.dart';
 
 class MyForms extends StatefulWidget {
   const MyForms({super.key});
@@ -13,8 +13,7 @@ class MyForms extends StatefulWidget {
 class _MyFormsState extends State<MyForms> {
 
 
-  String strST = "";
-  String strIM = "";
+  String strRadio = "";
 
   TextEditingController controllerNome = TextEditingController();
   TextEditingController controllerEmail = TextEditingController();
@@ -23,6 +22,9 @@ class _MyFormsState extends State<MyForms> {
   bool boolLiteraturaAfricana = false;
   bool boolArtes = false;
   bool boolEnvioNotificacoes = false;
+
+  List<Interessado> listaInteressados =[];
+  List<String> interesse =[];
 
   @override
   Widget build(BuildContext context) {
@@ -33,16 +35,20 @@ class _MyFormsState extends State<MyForms> {
         backgroundColor: Colors.blue,
       ),
 
-      body: Container(
+      body: SingleChildScrollView(
+        child:Container(
         padding: EdgeInsets.all(10),
-
         child: Column(children: [
           
-          Row(mainAxisAlignment: MainAxisAlignment.spaceAround,children: [
+          SizedBox(height: 30,),
 
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
+            Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7qA3AsbBrzLTXegdY-L8xC3FBUMbiqJ1qqF2t1UOoRncel51DfSoogSPkmdcWWmRVdPs&usqp=CAU", height: 130,),
+            Image.network("https://i.pinimg.com/474x/a1/b0/d3/a1b0d3f92d2a24f7886d9292ace0a2c7.jpg", height: 130,),
+            Image.network("https://i.pinimg.com/236x/1c/39/06/1c390656f302f231f4552c036602c19b.jpg", height: 130,)
           ],),
 
-          SizedBox(height: 30,),
+          SizedBox(height: 50,),
 
           TextField(
             controller: controllerNome,
@@ -63,27 +69,32 @@ class _MyFormsState extends State<MyForms> {
               fillColor: Colors.white,
               labelText: "Email",
           ),),
+          SizedBox(height: 10,),
+          Divider(),
 
-          SizedBox(height: 15,),
+          SizedBox(height: 40,),
 
-          Text("Tipo de Curso", style: TextStyle(fontSize: 14, color: Colors.black, ),),
+          Row(mainAxisAlignment: MainAxisAlignment.start,children: [
+            Text("Tipo de Curso", style: TextStyle(fontSize: 20, color: Colors.black,fontWeight: FontWeight.bold ),),
+          ],),
+          SizedBox(height: 10,),
           Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text("Somente Técnico"),
+            Text("Somente Técnico", style: TextStyle(fontSize: 16),),
             Radio(
               value: "Somente Técnico",
-              groupValue: strST,
+              groupValue: strRadio,
               onChanged: (value) {
-                  strST = value!;
+                  strRadio = value!;
                   setState(() {
                     
                   });
               },),
-            Text("Integrado ao Médio"),
+            Text("Integrado ao Médio", style: TextStyle(fontSize: 16),),
             Radio(
               value: "Integrado ao Médio",
-              groupValue: strIM,
+              groupValue: strRadio,
               onChanged: (value) {
-                  strIM = value!;
+                  strRadio = value!;
                   setState(() {
                     
                   });
@@ -92,12 +103,14 @@ class _MyFormsState extends State<MyForms> {
 
           Divider(),
 
-          SizedBox(height: 15,),
+          SizedBox(height: 40,),
 
-          Text("Oficinas de Interesse:"),
-          SizedBox(height: 5,),
+          Row(mainAxisAlignment: MainAxisAlignment.start,children: [
+            Text("Oficinas de Interesse:", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+          ],),
+          SizedBox(height: 10,),
           CheckboxListTile(
-              title: Text("Escrita científica"),
+              title: Text("Escrita científica", style: TextStyle(fontSize: 16),),
               value: boolEscritaCientifica, 
               onChanged: (value) {
                 boolEscritaCientifica = value!;
@@ -106,7 +119,7 @@ class _MyFormsState extends State<MyForms> {
                 });
               },),
           CheckboxListTile(
-              title: Text("Literatura Africana"),
+              title: Text("Literatura Africana", style: TextStyle(fontSize: 16),),
               value: boolLiteraturaAfricana, 
               onChanged: (value) {
                 boolLiteraturaAfricana = value!;
@@ -115,7 +128,7 @@ class _MyFormsState extends State<MyForms> {
                 });
               },),
           CheckboxListTile(
-              title: Text("Artes"),
+              title: Text("Artes", style: TextStyle(fontSize: 16),),
               value: boolArtes, 
               onChanged: (value) {
                 boolArtes = value!;
@@ -126,8 +139,10 @@ class _MyFormsState extends State<MyForms> {
 
             Divider(),
 
+            SizedBox(height: 40,),
+
               SwitchListTile(
-              title: Text("Permitir envio de notificações no email."),
+              title: Text("Permitir envio de notificações no email.", style: TextStyle(fontSize: 16),),
               value: boolEnvioNotificacoes, 
               onChanged: (value) {
                 boolEnvioNotificacoes = value;
@@ -140,14 +155,56 @@ class _MyFormsState extends State<MyForms> {
 
               Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
                 ElevatedButton(onPressed: () {
-                  
+                  if(boolEscritaCientifica){
+                    interesse.add("Escrita Científica");
+                  }
+                  if(boolLiteraturaAfricana){
+                    interesse.add("Literatura Africana");
+                  }
+                  if(boolArtes){
+                    interesse.add("Artes");
+                  }
+                  Interessado inte;
+                  listaInteressados.add(inte = Interessado(controllerNome.text, controllerEmail.text, strRadio, boolEnvioNotificacoes, interesse));
+                  limpar();
+                  interesse = [];
+                  mostrar();
+
+                  setState(() {
+                    
+                  });
                 }, child: Text("Enviar")),
                 ElevatedButton(onPressed: () {
-                  
+                  limpar();
+                  setState(() {
+
+                  });
                 }, child: Text("Cancelar"))
               ],)
         ],),
       ),
-    );
+    ));
+  }
+ 
+    void limpar(){
+      controllerNome = TextEditingController();
+      controllerEmail = TextEditingController();
+      strRadio = "";
+      boolArtes = false;
+      boolEnvioNotificacoes = false;
+      boolEscritaCientifica = false;
+      boolLiteraturaAfricana = false;
+  }
+
+  void mostrar(){
+    for(var interes in listaInteressados){
+      print("Nome: " + interes.nome);
+      print("Email: " + interes.email);
+      print("Tipo do Curso: " + interes.tipoCurso);
+      print("Oficinas interessadas: " + interes.oficinas.toString());
+      print("Notificações: " + interes.not.toString());
+      print("----------------------");
+    }
   }
 }
+
--- flutter_application_exer_forms_radio_switch/lib/interessado.dart
+++ flutter_application_exer_forms_radio_switch/lib/interessado.dart
@@ -1,5 +1,33 @@
+// ignore_for_file: unnecessary_this
+
 class Interessado{
 
-  String nome = "";
-  String email = "";
+  String _nome = "";
+  String _email = "";
+  String _tipoCurso = "";
+  bool _not = false;
+  List<String> _oficinas = [];
+
+  Interessado(String nome, String email, String tipoCurso, bool not, List<String> oficinas){
+    this._nome = nome;
+    this._email = email;
+    this._tipoCurso = tipoCurso;
+    this._not = not;
+    this._oficinas = oficinas;
+  }
+
+  String get nome => this._nome;
+  set nome( value) => this._nome = value;
+
+  String get email => this._email;
+  set email( value) => this._email = value;
+
+  String get tipoCurso => this._tipoCurso;
+  set tipoCurso( value) => this._tipoCurso = value;
+
+  bool get not => this._not;
+  set not( value) => this._not = value;
+
+  List<String> get oficinas => this._oficinas;
+
 }
