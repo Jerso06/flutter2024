92b733f56e9213189b7c5fc616ec051c7e44ea2d
Jerso06 <jedsonhenrique2006@gmail.com>
2024-09-10 21:22:03 -0300

    Finalização exercício API Star Wars (10/09)

--- flutter_application_api_ex_entregar/lib/index.dart
+++ flutter_application_api_ex_entregar/lib/index.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, unnecessary_import, unused_import
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, unnecessary_import, unused_import, prefer_interpolation_to_compose_strings, prefer_typing_uninitialized_variables
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -13,28 +13,74 @@ class MyIndex extends StatefulWidget {
 }
 
 class _MyIndexState extends State<MyIndex> {
-  String dadosApi = "";
+  String dadosApi = "teste";
+  String strRadio = "";
+  TextEditingController controllerId = TextEditingController();
+
+  //people
+  var namePeople;
+  var height;
+  var mass;
+  var hairColor;
+  var skinColor;
+
+  //planets
+  var namePlanets;
+  var rotationPeriod;
+  var orbitalPeriod;
+  var diameter;
+  var climate;
+
+  //starships
+  var nameStarships;
+  var model;
+  var manufacturer;
+  var costInCredits;
+  var length;
 
   Future<void> request() async {
     var url;
     if(strRadio == "People"){
       url = Uri.parse("https://swapi.dev/api/people/${controllerId.text}");
-    }else if(strRadio == "Planet"){
+    }else if(strRadio == "Planets"){
       url = Uri.parse("https://swapi.dev/api/planets/${controllerId.text}");
     }else if(strRadio == "Starships"){
       url = Uri.parse("https://swapi.dev/api/starships/${controllerId.text}");
     }
     http.Response response;
-      response = await http.get(url);
-      dadosApi = response.body;
+    response = await http.get(url);
+    dadosApi = response.body;
       if(response.statusCode == 200){
-
+        Map<String, dynamic> formatedData = jsonDecode(dadosApi);
+        if(strRadio == "People"){
+          namePeople = formatedData["name"];
+          height = formatedData["height"];
+          mass = formatedData["mass"];
+          hairColor = formatedData["hair_color"];
+          skinColor = formatedData["skin_color"];
+          dadosApi = "Name: " + namePeople + "\nHeight: " + height + "\nMass: " + mass + "\nHair Color: " + hairColor + "\nSkin Color: " + skinColor;
+        }
+        else if (strRadio == "Planets"){
+          namePlanets = formatedData["name"];
+          rotationPeriod = formatedData["rotation_period"];
+          orbitalPeriod = formatedData["orbital_period"];
+          diameter = formatedData["diameter"];
+          climate = formatedData["climate"];
+          dadosApi = "Name: " + namePlanets + "\nRotation Period: " + rotationPeriod + "\nOrbital Period: " + orbitalPeriod + "\nDiameter: " + diameter + "\nClimate: " + climate; 
+        }
+        else if(strRadio == "Starships"){
+          nameStarships = formatedData["name"];
+          model = formatedData["model"];
+          manufacturer = formatedData["manufacturer"];
+          costInCredits = formatedData["cost_in_credits"];
+          length = formatedData["length"];
+          dadosApi = "Name: "+ nameStarships + "\nModel: " + model + "\nManufacturer: "+ manufacturer + "\nCost In Credits: " + costInCredits + "\nLength: " + length;
+        }
       }
+    setState(() {
+        
+    });
   }
-
-  String strRadio = "";
-
-  TextEditingController controllerId = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -45,77 +91,88 @@ class _MyIndexState extends State<MyIndex> {
         backgroundColor: Colors.black,
       ),
       body: Container(
+        color: Colors.black,
         padding: EdgeInsets.all(10),
         child: Center(child: Column(children: [
-        SizedBox(height: 20,),
+          SizedBox(height: 20,),
 
-        TextFormField(
-          controller: controllerId,
-           decoration: InputDecoration(
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-            filled: true,
-            fillColor: Colors.white,
-            labelText: "Id",
+          TextFormField(
+            controller: controllerId,
+            style: TextStyle(color: Colors.yellow),
+            decoration: InputDecoration(
+              focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(50),borderSide: BorderSide(color: Colors.yellow)),
+              enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(50),borderSide: BorderSide(color: Colors.yellow)),
+              filled: true,
+              fillColor: Colors.black,
+              labelText: "ID",
+              labelStyle: TextStyle(color: Colors.yellow, ),
+            ),
           ),
-        ),
 
-        SizedBox(height: 20,),
+          SizedBox(height: 20,),
 
-        Text("Tipo de Dados:",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-        Row(mainAxisAlignment: MainAxisAlignment.start,children: [
-          Text("People", style: TextStyle(fontSize: 16),),
-          Radio(
-            value: "People",
-            groupValue: strRadio,
-            onChanged: (value) {
-              strRadio = value!;
-              setState(() {
-                
-              });
-            },),
-        ],),
-        SizedBox(height: 15,),
-        Row(mainAxisAlignment: MainAxisAlignment.start,children: [
-          Text("Planet", style: TextStyle(fontSize: 16),),
-          Radio(
-            value: "Planet",
-            groupValue: strRadio,
-            onChanged: (value) {
-              strRadio = value!;
-              setState(() {
-                
-              });
-            },),
-        ],),
-        SizedBox(height: 15,),
-        Row(mainAxisAlignment: MainAxisAlignment.start,children: [
-          Text("Starships", style: TextStyle(fontSize: 16),),
-          Radio(
-            value: "Starships",
-            groupValue: strRadio,
-            onChanged: (value) {
-              strRadio = value!;
-              setState(() {
-                
-              });
-            },)
-        ],),
+          Text("Tipo de Dados:",style: TextStyle(color: Colors.yellow, fontSize: 20, fontWeight: FontWeight.bold),),
+          Row(mainAxisAlignment: MainAxisAlignment.start,children: [
+            Text("People", style: TextStyle(fontSize: 16, color: Colors.yellow),),
+            Radio(
+              value: "People",
+              activeColor: Colors.yellow,
+              groupValue: strRadio,
+              onChanged: (value) {
+                strRadio = value!;
+                setState(() {
+                  
+                });
+              },),
+          ],),
+          SizedBox(height: 15,),
+          Row(mainAxisAlignment: MainAxisAlignment.start,children: [
+            Text("Planets", style: TextStyle(fontSize: 16, color: Colors.yellow),),
+            Radio(
+              value: "Planets",
+              groupValue: strRadio,
+              activeColor: Colors.yellow,
+              onChanged: (value) {
+                strRadio = value!;
+                setState(() {
+                  
+                });
+              },),
+          ],),
+          SizedBox(height: 15,),
+          Row(mainAxisAlignment: MainAxisAlignment.start,children: [
+            Text("Starships", style: TextStyle(fontSize: 16,color: Colors.yellow),),
+            Radio(
+              value: "Starships",
+              groupValue: strRadio,
+              activeColor: Colors.yellow,
+              onChanged: (value) {
+                strRadio = value!;
+                setState(() {
+                  
+                });
+              },)
+          ],),
 
-        SizedBox(height: 20,),
+          SizedBox(height: 20,),
 
-        SizedBox(
-          height: 50,
-          width: 200,
-          child: ElevatedButton(style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.black,
-            backgroundColor: Colors.yellow,
-            textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+          SizedBox(
+            height: 50,
+            width: 200,
+            child: ElevatedButton(style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.yellow,
+              textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+            ),
+              onPressed: () {
+                request();
+              },
+              child: Text("Buscar Dados")),
           ),
-            onPressed: () {
-              
-            },
-            child: Text("Buscar Dados")),
-        )
+
+          SizedBox(height: 20,),
+
+          Text(dadosApi, style: TextStyle(color: Colors.yellow, fontSize: 22, fontWeight: FontWeight.bold),)
       ],),),
     ));
   }
